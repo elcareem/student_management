719ecf8a28ffeff1d3dc5623245239a0013e8871
your_name <andrewmiracle100@gmail.com>
2025-08-26 17:31:08 +0100

    add update & display student functionality and modify add & delete student function

--- student_management.py
+++ student_management.py
@@ -5,7 +5,7 @@ student_db = {
 
 def add_student():
 	name = input("Enter your name>>> ")
-	age = input("Enter your age>>> ")
+	age = int(input("Enter your age>>> "))
 	department = input("Enter your department>>> ")
 	
 	if not student_db:
@@ -14,45 +14,71 @@ def add_student():
 	else:
 		id_number = len(student_db) + 1
 		student_db.update({id_number: {'name': name, 'age': age, 'department': department}})
-		print(f"Your student id number is {id_number}  ")
+		print(f"Your student id number is {id_number}")
+
 def delete_student():
-	student_id = "Enter the id of the student you want to delete>>>"
-	removed_id = student_db.pop(student_id, 'Not found')
-	print(f'Student with id of {removed_id} has been removed')
+	student_id = int(input("Enter the id of the student you want to delete>>> "))
+	if student_id in student_db.keys():
+		del student_db[student_id]
+		print(f'Student with id of {student_id} has been removed')
+	else:
+		print("Student not found")
+
 	
 def update_student():
-	student
+	student_id = int(input("Enter the id of the student whose information you want to update>>> "))
+	if student_id in student_db.keys():
+		name = input("Enter name>>> ")
+		age = input("Enter age>>> ")
+		department = input("Enter department>>> ")
+		student_db.update({student_id: {'name': name, 'age': age, 'department': department}})		
+		print(f'''
+Student with id no {student_id} updated succesfully 
+{student_db[student_id]}
+		''')
+
+	else:
+		print(f"Student with id {student_id} not found") 
+
+
+
+def display_one_student():
+	student_id = int(input("Enter student id>>> "))
+	if student_id in student_db.keys():
+		print(student_db[student_id])
+	else:
+		print("Student not found")
+	 
+def display_all_students():
+	for student in student_db.keys():
+		print(student_db[student])
+
+
 def start():
-	display_info = '''
+	print( '''
 STUDENT MANAGEMENT SYSTEM
---------------------------
-Enter commands:
+`--------------------------
+Available commands:
 1 to Add Student
 2 to Delete Student
 3 to Update Student
 4 to Display One Student
 5 to Display All Students
-
-'''
-
-	command = input(display_info)
+	''')
 
 	while True:
-		if command == 1:
-			pass
-		elif command == 2:
-			pass 
-		elif command == 3:
-			pass
-		elif command == 4:
-			pass
-		elif command == 5:
-			pass 
-		elif command == 6: 
-			pass
-		elif command == 7:
-			pass
+		command = input("Enter command>>> ")
+		if command == '1':
+			add_student()	
+		elif command == '2':
+			delete_student() 
+		elif command == '3':
+			update_student()
+		elif command == '4':
+			display_one_student()
+		elif command == '5':
+			display_all_students()
 		else:
-			"Invalid command" 
+			print("Invalid command") 
 
-  
+start() 
